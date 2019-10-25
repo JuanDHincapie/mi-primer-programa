cc9d3089584d2b3161f688be7ed839945009b524
juandeaadoradores@gmail.com <Judahiso_91>
2019-10-24 20:40:42 -0500

    prueba_ejercicio_for_1

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -4,17 +4,15 @@ Crea un programa quesea capaz de contar las letras mayusculas dentro de una fras
 
 frase_usuario = input("escribe una frase que contenga mayusculas dentro: ")
 
-mayusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"]
+mayusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+              "V", "W", "X", "Y", "Z"]
 
 n_mayusculas = 0
 n_minusculas = 0
 for letra in frase_usuario:
     if letra in mayusculas:
-        n_mayusculas +=1
+        n_mayusculas += 1
     else:
-        n_minusculas +=1
+        n_minusculas += 1
 print("el numero de mayusculas es de {}".format(n_mayusculas))
 print("el numero de minusculas es de {}".format(n_minusculas))
-
-
-
